138df5d3f310add5208e4375e9fe266724e3f37f
jinha <jinha5959@naver.com>
2025-10-29 15:30:25 +0900

    pandas review v0.2

---
+++ week09.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+titanic = sns.load_dataset('titanic')
+# print(titanic.info())
+# print(titanic.tali(3))
+print(titanic.query('age >= 70'))
+
+sns.scatterplot(x='age', y='fare', data=titanic)
+plt.plot()
+plt.show()
